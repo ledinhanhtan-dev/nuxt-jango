edeb6acf05bcac3d37129bd2cda34f58a2707752
ledinhanhtan-dev <ledinhanhtan.dev@gmail.com>
2022-05-10 23:38:39 +0700

    feat: django models

--- server/server/urls.py
+++ server/server/urls.py
@@ -13,12 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+import settings
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('djoser.urls')),
     path('api/', include('djoser.urls.authtoken')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
