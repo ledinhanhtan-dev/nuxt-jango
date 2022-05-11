445f4195e32c225fb14bc8b2a79da051d5bc38d7
tan.le <tan.le@paradox.ai>
2022-05-11 10:33:05 +0700

    feat: django

--- server/server/urls.py
+++ server/server/urls.py
@@ -16,11 +16,12 @@ Including another URLconf
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-import settings
+from django.conf import settings
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('djoser.urls')),
     path('api/', include('djoser.urls.authtoken')),
+    path('api/', include('product.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
